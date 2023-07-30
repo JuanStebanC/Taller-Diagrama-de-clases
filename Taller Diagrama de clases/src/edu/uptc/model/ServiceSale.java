c719b0ca32360eaf14cac7773727230a70815547
JuanStebanC <juanstebanleoncardenas@gmail.com>
2023-07-29 20:02:27 -0500

    ServiceSale
    Se crean los métodos de la clase ServiceSale

--- Taller Diagrama de clases/src/edu/uptc/model/ServiceSale.java
+++ Taller Diagrama de clases/src/edu/uptc/model/ServiceSale.java
@@ -1,5 +1,19 @@
 package edu.uptc.model;
 
 public class ServiceSale {
+	private Sale sale;
 
+	public ServiceSale(Sale sale) {
+		super();
+		this.sale = sale;
+	}
+
+	public Sale getSale() {
+		return sale;
+	}
+
+	public void setSale(Sale sale) {
+		this.sale = sale;
+	}
+	
 }
