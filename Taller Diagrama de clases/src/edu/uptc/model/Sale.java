cdf5e7dbf1a21a528a6fbf278d109408b1ce4601
JuanStebanC <juanstebanleoncardenas@gmail.com>
2023-08-03 09:13:09 -0500

    Corrección clase Sale
    Se realiza la corrección de la clase Sale debido a un error en la comprensión del funcionamiento de sus métodos

--- Taller Diagrama de clases/src/edu/uptc/model/Sale.java
+++ Taller Diagrama de clases/src/edu/uptc/model/Sale.java
@@ -22,32 +22,45 @@ public class Sale {
 	}
 	public double calcDiscount() {
 		double discount = 0;
+		/*- Cantidad entre 5 y 10 5%
+		- Entre 11 y 20 10%
+		- Entre 21 y 50 20%
+		- Mayor a 50 30%*/
+		if (this.getCant() >= 5 && this.getCant() <= 10) {
+			discount = 0.05;
+		} else if (this.getCant() >= 11 && this.getCant() <= 20) {
+			discount = 0.1;
+		} else if (this.getCant() >= 21 && this.getCant() <= 50) {
+			discount = 0.2;
+		} else if (this.getCant() > 50) {
+			discount = 0.3;
+		}
+		return discount;
+	}
+	public double getTotalSale() {
+		double total = (product.getValue())-(calcDiscount())+(calcIva());
+		return total;
+				
+	}
+	public double calcIva() {
+		double iva = 0;
 		switch (product.getTypeProduct().toString()) {
 		case "LICORES":
-			discount=(product.getValue())*0.19;
+			iva=(product.getValue())*0.19;
 			break;
 		case "VIVERES":
-			discount=(product.getValue())*0.08;
+			iva=(product.getValue())*0.08;
 			break;
 		case "ASEO":
-			discount=(product.getValue())*0.14;
+			iva=(product.getValue())*0.14;
 			break;
 		case "RANCHO":
-			discount=(product.getValue())*0.19;
+			iva=(product.getValue())*0.19;
 			break;
 		case "MEDICINAS":
 			break;
 		default:
 		}
-		return discount;
-	}
-	public double getTotalSale() {
-		double total = (product.getValue())-(calcDiscount());
-		return total;
-				
-	}
-	public double calcIva() {
-		double iva = (product.getValue()) * 0.19;
 		return iva;
 	}
 }
